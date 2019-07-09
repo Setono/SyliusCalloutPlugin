2941dfbef9b19bd0f25d1f6976c22547a8f20d8d
Joachim Løvgaard <joachim@loevgaard.dk>
2019-07-09 12:50:51 +0200

    fixed missing parameters

--- src/DependencyInjection/SetonoSyliusCalloutExtension.php
+++ src/DependencyInjection/SetonoSyliusCalloutExtension.php
@@ -4,44 +4,20 @@ declare(strict_types=1);
 
 namespace Setono\SyliusCalloutPlugin\DependencyInjection;
 
-use Setono\SyliusCalloutPlugin\Message\Command\AssignProductCallouts;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class SetonoSyliusCalloutExtension extends AbstractResourceExtension implements PrependExtensionInterface
+final class SetonoSyliusCalloutExtension extends AbstractResourceExtension
 {
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $container->setParameter('setono_sylius_callout.messenger.command_bus', $config['messenger']['command_bus']);
-        $container->setParameter('setono_sylius_callout.messenger.transport', $config['messenger']['transport']);
-
         $loader->load('services.xml');
 
         $this->registerResources('setono_sylius_callout', $config['driver'], $config['resources'], $container);
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $container->getExtensionConfig($this->getAlias()));
-
-        $transport = $config['messenger']['transport'];
-
-        if (null === $transport) {
-            return;
-        }
-
-        $container->prependExtensionConfig('framework', [
-            'messenger' => [
-                'routing' => [
-                    AssignProductCallouts::class => $transport,
-                ],
-            ],
-        ]);
     }
 }
