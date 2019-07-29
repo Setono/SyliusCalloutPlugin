de4bfc54d630a5cb1c81c6d3f84bf3d4f6c8413c
Igor Mukhin <igor.mukhin@gmail.com>
2019-07-29 14:16:18 +0300

    Added: CalloutAssignAction

---
+++ src/Controller/Action/CalloutAssignAction.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusCalloutPlugin\Controller\Action;
+
+use Setono\SyliusCalloutPlugin\Callout\Assigner\CalloutAssignerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+final class CalloutAssignAction
+{
+    /** @var CalloutAssignerInterface */
+    private $calloutAssigner;
+
+    /** @var SessionInterface */
+    private $session;
+
+    /** @var TranslatorInterface */
+    private $translator;
+
+    /** @var RouterInterface */
+    private $router;
+
+    public function __construct(
+        CalloutAssignerInterface $calloutAssigner,
+        SessionInterface $session,
+        TranslatorInterface $translator,
+        RouterInterface $router
+    ) {
+        $this->calloutAssigner = $calloutAssigner;
+        $this->session = $session;
+        $this->translator = $translator;
+        $this->router = $router;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $this->calloutAssigner->assign();
+
+        /** @var FlashBagInterface $flashBag */
+        $flashBag = $this->session->getBag('flashes');
+        $flashBag->add(
+            'success',
+            $this->translator->trans('setono_sylius_callout.callout.assign_started', [], 'flashes')
+        );
+
+        return new RedirectResponse(
+            $this->router->generate('setono_sylius_callout_admin_callout_index')
+        );
+    }
+}
