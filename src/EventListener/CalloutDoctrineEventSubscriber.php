26bf70a1fe6991639044b45f92384f01117b6e83
Igor Mukhin <igor.mukhin@gmail.com>
2019-10-10 19:30:54 +0300

    Fixed: Event listener to handle recalculation if no_rules_eligible=true

--- src/EventListener/CalloutDoctrineEventSubscriber.php
+++ src/EventListener/CalloutDoctrineEventSubscriber.php
@@ -5,14 +5,30 @@ declare(strict_types=1);
 namespace Setono\SyliusCalloutPlugin\EventListener;
 
 use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Events;
 use Doctrine\ORM\PersistentCollection;
 use Setono\SyliusCalloutPlugin\Model\CalloutInterface;
 use Setono\SyliusCalloutPlugin\Model\CalloutRuleInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 final class CalloutDoctrineEventSubscriber extends AbstractCalloutDoctrineEventSubscriber implements EventSubscriber
 {
+    /** @var bool */
+    private $isNoRulesEligible;
+
+    public function __construct(
+        EntityManager $calloutManager,
+        MessageBusInterface $commandBus,
+        bool $manualTriggering = false,
+        bool $isNoRulesEligible = false
+    ) {
+        parent::__construct($calloutManager, $commandBus, $manualTriggering);
+
+        $this->isNoRulesEligible = $isNoRulesEligible;
+    }
+
     public function getSubscribedEvents()
     {
         return [
@@ -25,6 +41,16 @@ final class CalloutDoctrineEventSubscriber extends AbstractCalloutDoctrineEventS
     {
         $em = $args->getEntityManager();
         $uow = $em->getUnitOfWork();
+
+        if ($this->isNoRulesEligible) {
+            foreach ($uow->getScheduledEntityInsertions() as $entity) {
+                if (!$entity instanceof CalloutInterface) {
+                    continue;
+                }
+
+                $this->scheduleCalloutUpdate($entity);
+            }
+        }
 
         // Here we need only rules collection changes to track as far as
         // creating/updating Callout itself (without rules) not require
