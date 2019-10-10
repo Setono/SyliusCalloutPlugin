c4c03e32a20a3d0dc33e5824e164c7df3a284e55
Igor Mukhin <igor.mukhin@gmail.com>
2019-10-10 17:39:54 +0300

    Added: Is new rule

---
+++ src/Callout/Checker/Rule/IsNewProductCalloutRuleChecker.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusCalloutPlugin\Callout\Checker\Rule;
+
+use DateTime;
+use DateTimeInterface;
+use Setono\SyliusCalloutPlugin\Model\CalloutsAwareInterface;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Promotion\Exception\UnsupportedTypeException;
+use Webmozart\Assert\Assert;
+
+final class IsNewProductCalloutRuleChecker implements ProductCalloutRuleCheckerInterface
+{
+    public const TYPE = 'is_new';
+
+    /** @var DateTimeInterface */
+    private $now;
+
+    public function __construct(?DateTimeInterface $now = null)
+    {
+        if (null === $now) {
+            $now = new DateTime('now');
+        }
+
+        $this->now = $now;
+    }
+
+    public function isEligible(CalloutsAwareInterface $product, array $configuration): bool
+    {
+        if (!$product instanceof ProductInterface) {
+            throw new UnsupportedTypeException($product, ProductInterface::class);
+        }
+
+        Assert::keyExists($configuration, 'days');
+        Assert::numeric($configuration['days']);
+
+        $createdAt = $product->getCreatedAt();
+        if (null === $createdAt) {
+            return false;
+        }
+
+        $interval = $this->now->diff($createdAt);
+
+        return (int) $interval->format('%a') <= (int) $configuration['days'];
+    }
+}
