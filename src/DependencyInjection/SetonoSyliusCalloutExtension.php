014068d8c4a2c192c75b1218acd69a1b6a25821a
Roshyo <stefdecock68@gmail.com>
2021-07-12 15:09:08 +0200

    Remove Doctrine migration 3

--- src/DependencyInjection/SetonoSyliusCalloutExtension.php
+++ src/DependencyInjection/SetonoSyliusCalloutExtension.php
@@ -4,17 +4,13 @@ declare(strict_types=1);
 
 namespace Setono\SyliusCalloutPlugin\DependencyInjection;
 
-use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class SetonoSyliusCalloutExtension extends AbstractResourceExtension implements PrependExtensionInterface
+final class SetonoSyliusCalloutExtension extends AbstractResourceExtension
 {
-    use PrependDoctrineMigrationsTrait;
-
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
@@ -25,25 +21,5 @@ final class SetonoSyliusCalloutExtension extends AbstractResourceExtension imple
         $container->setParameter('setono_sylius_callout.manual_triggering', $config['manual_triggering']);
         $container->setParameter('setono_sylius_callout.no_rules_eligible', $config['no_rules_eligible']);
         $this->registerResources('setono_sylius_callout', $config['driver'], $config['resources'], $container);
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        $this->prependDoctrineMigrations($container);
-    }
-
-    protected function getMigrationsNamespace(): string
-    {
-        return 'Setono\SyliusCalloutPlugin\Migrations';
-    }
-
-    protected function getMigrationsDirectory(): string
-    {
-        return '@SetonoSyliusCalloutPlugin/Migrations';
-    }
-
-    protected function getNamespacesOfMigrationsExecutedBefore(): array
-    {
-        return ['Sylius\Bundle\CoreBundle\Migrations'];
     }
 }
