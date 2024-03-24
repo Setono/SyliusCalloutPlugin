f9bc255ff521fd1ac874b1f3aefccf5189ae49c5
niph <piotr@opiola.eu>
2024-03-24 14:34:12 +0100

    Add ProductTrait attribute mapping

--- src/Model/ProductTrait.php
+++ src/Model/ProductTrait.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 trait ProductTrait
 {
     /** @ORM\Column(type="json", nullable=true) */
+    #[ORM\Column(type: 'json', nullable: true)]
     protected ?array $preQualifiedCallouts = null;
 
     public function getPreQualifiedCallouts(): array
