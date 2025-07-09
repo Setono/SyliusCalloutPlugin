ce9d28d1c599341c178c535a251f5cc55ce3950a
Joachim Løvgaard <joachim@loevgaard.dk>
2025-07-09 14:54:52 +0200

    Add false positives

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('sylius/grid-bundle', [ErrorType::UNUSED_DEPENDENCY]) // We use this for defining our grid in the admin
+    ->ignoreErrorsOnPackage('symfony/twig-bundle', [ErrorType::UNUSED_DEPENDENCY]) // This creates the 'twig' service which we rely on
 ;
